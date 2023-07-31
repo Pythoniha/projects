41dff8ba8dd061eb16dbe3c4d2bad8d8b60c3ab1
Pythoniha.ir <70705542+Pythoniha@users.noreply.github.com>
2023-07-31 17:01:57 +0330

    Create main.py

---
+++ 9/main.py
@@ -0,0 +1,16 @@
+#Pythoniha.ir | @Pythonihair | #MohamadReza Hassani
+import json
+
+if __name__ == '__main__':
+    try:
+        with open('in.json', 'r') as f:
+            data = json.loads(f.read())
+
+        output = ','.join([*data[0]])
+        for obj in data:
+            output += f'\n{obj["Name"]},{obj["age"]},{obj["birthyear"]}'
+
+        with open('output.csv', 'w') as f:
+            f.write(output)
+    except Exception as ex:
+        print(f'Error: {str(ex)}')
